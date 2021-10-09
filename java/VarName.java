8b289930027ef884348f0724290fb4a3a009089d
Hetal Nayi <bhumi18@hotmail.com>
2021-10-09 14:34:12 +0100

    This is my First commit

---
+++ java/VarName.java
@@ -0,0 +1,3 @@
+public class VarName {
+    public void
+}
---
+++ java/com/demo/nopcommerce/HomePage.java
@@ -0,0 +1,12 @@
+package com.demo.nopcommerce;
+
+public class HomePage {
+
+    //Main Method to execute the program
+    public static void main(String[] args) {
+
+        //Print statement to print some text.
+        System.out.println("My First Java Program");
+    }
+
+}
